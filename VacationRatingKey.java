5fadd8fe88b134891c48accab2fe799ff2e236b2
JonasEwn <jonas.esswein@gmail.com>
2022-06-16 16:23:35 +0200

    Update VacationRatingKey.java

--- VacationRatingKey.java
+++ VacationRatingKey.java
@@ -9,6 +9,7 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class VacationRatingKey implements Serializable{
 
+	//Ist nicht unbedingt notwendig
 	private static final long serialVersionUID = 1L;
 
 	@Column(name = "id")
